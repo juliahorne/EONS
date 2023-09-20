de1fe34c42c36fed4c13dbe8de121e2319dffdba
Julia Horne <90068610+juliahorne@users.noreply.github.com>
2023-09-19 17:17:36 -0700

    Update TempParam.m

--- TempParam.m
+++ TempParam.m
@@ -34,14 +34,14 @@ pCO2           = v.atm.CO2pal./v.atm.mol; % mixing ratio of CO2 in modern atm (a
 % for the modern day:
 Fs_m = solarrad(v.S.Pref,v);        % W/m2; modern solar flux
 Fbb_m = blackbody(Ts,v);            % W/m2; modern black body IR flux
-GFm = RFInterp(pCO2,'CO2',v);       % W/m2; modern GHF at pCO2 = 400 ppm
+GFm = RFInterp(pCO2,'CO2',v.bb);    % W/m2; modern GHF at pCO2 = 400 ppm
 
 % for the Archean:
 Fs_a = solarrad(v.S.Aref,v);        % W/m2; archean solar flux
 Fbb_a = Fbb_m;                      % W/m2; archean black body IR flux at modern temp
 switch method
     case 'A'
-        GFa = RFInterp(100*pCO2,'CO2',v); % W/m2; archean GHF at pCO2 = 30,000 ppm
+        GFa = RFInterp(v.bb.aPAL*pCO2,'CO2',v.bb); % W/m2; archean GHF at pCO2 (assuming 10^2-10^4 x PAL - Catling + Zanhle, 2020)
     otherwise
         GFa = GFm + 30;             %  W/m2; archean GHF at pCO2 = 15,000 ppm
 end
@@ -60,7 +60,7 @@ yir_m = ThermalIR(Fbb_m,Fs_m);      % modern thermal IR optical depth
 % and GFa
 yir_a = ThermalIR(Fbb_a,Fs_a);      % archean thermal IR optical depth
 
-%% Calculate the constants (aT, bT) that equate the greenhouse forcing for the two eras
+%% Calculate the constants (a1, b1) that equate the two eras
 % now solve for the equilibrating constants from:
     % yir_a = a1Ts + b1GF_a     and     yir_m = a1Ts + b1GF_m
     % a1Ts = yir_a - b1GF_a     and     a1Ts = yir_m - b1GF_m 
@@ -72,7 +72,7 @@ b1 = bconstant(yir_a,yir_m,GFa,GFm);  % thermal IR optical depth GHG constant
 
 % re-enter into first equation to find a1 for modern Earth (should == Archean)
     % yir_m = a1Ts + b1GFm
-a1 = aconstant(yir_m,b1,GFm,Ts,v.atm.Tx);% thermal IR optical depth Temp constant
+a1 = aconstant(yir_m,b1,GFm,Ts,v);% thermal IR optical depth Temp constant
 
 % plot the relationship of yir and greenhouse forcing
 switch PLOT
@@ -89,7 +89,7 @@ switch PLOT
             'location','southeast','NumColumns',2); 
 end
 
-%% calculate the pressure broadening parameter q 
+%% calculate q 
 % % From Goldblatt et al. (2009) : doubling PAN @ 2.5 Ga = +4.4C
 % Y = (N/N_0)^q  --> the portion of incoming solar flux retained by N-pressure broadening of absorption bands
 % with no haze effects:
@@ -155,7 +155,7 @@ switch PLOT
         title('Pressure Broadening effect at different pCO2 levels in Archean');
 end
 
-%% If we want to include aerosol haze, calculate y_hz constants
+%% calculate y_hz constants
 % From McKay et al. (1999) and DeWitt et al. (2009): y_hz = 1-exp(-B g y_ir)
 % where 0.28 < g < 0.5 (for Earth-like hazes, conservative est = 0.5) 
 % and B = 1.5 (average enhancement factor from Trainer et al. 2006)
@@ -213,38 +213,22 @@ switch PLOT
         printplotpdf(20,20,'Tparam_GammaIRvsT'); 
 end
 
-% % based on the above figure, we want to assume haze behaviour much closer
-% % to that observed on Titan in order to set our y_hz parameterization
-% % this means that we will assume that formed hazes are not strongly opaque
-% % in the visible, but only moderately (so dT is not >15 K under haze!)
-% yhzm = 1-exp(-v.atm.hz.beta.*v.atm.hz.g.*yir_m); % where g = 0.28
-% 
-% but we still want to relate this to CH4:CO2, so find the constants anyway!
-    % y_hz = a2(CH4/CO2) 
+% we use the same assumptions as Dewitt (2009) and Trainer (2006) for
+% behavior of haze on early Earth - the conservative limit of g = 0.5 (only
+% moderately reflective haze) used by Dewitt et al, the scaling factor for 
+% haze at CH4:CO2 = 1 used by Dewitt (2009) and Trainer (2006; beta), and
+% Dewitt's formation. 
+yhz = 1-exp(-1.5.*0.5.*yir_a);   % antigreenhouse factor on early Earth
 % therefore:
-    % a2 = y_hz./(CH4/CO2)
-% a2 = 1./0.1; % tuned so that at CH4:CO2 = 0.1, y_hz = 1 (ie. totally visibly opaque)
-% or just lightly opaque, y_hz = 0.5 (max)
-a2 = 0.5./0.1; 
-
-% see what happens just by varying a2 for CH4/CO2 = 0.1
-switch PLOT
-    case 'on'
-        a2list = 0.1:0.5:10; 
-        figure(); clf; 
-        for ia = 1:length(a2list)
-           yhza(ia) = a2list(ia).*0.1;  
-        end
-        plot(a2list,yhza); ylabel('y_{hz} param at CH4/CO2 = 0.1'); xlabel('a_2 param'); 
-end
-
+    % a2 = y_hz./(CH4/CO2) and Dewitt uses beta = 1.5 at CH4:CO2 = 1, so:
+a2 = yhz; 
 
 %% export all new constants within a structure
 tp.aT  = a1; 
 tp.ahz = a2; 
 tp.bT  = b1;
 tp.q   = qa; % the Goldblatt et al. 2009 paper was studying the Archean, so use this reference value! 
-tp.CS  = climatesensitivity(pCO2,tp.aT,tp.bT,v);    % find climate sensitivity (dK per CO2 doubling, 2.5-4
+tp.CS  = climatesensitivity(pCO2,tp,v);    % find climate sensitivity (dK per CO2 doubling, expect 2.5-4)
 
 end
 
@@ -282,11 +266,12 @@ function yir = ThermalIR(Fbb,Fs)
     yir = (4/3) .* ((Fbb./Fs) - 1); % fraction (IR opacity)
 end
 
-function acon = aconstant(yir,bcon,RF,T,Tx)
+function acon = aconstant(yir,bcon,RF,T,v)
 % simplified to exclude pressure broadening (Y == gamma)
     % y_ir = Y(a1 Ts + b1 RF) = a1 Ts + b1 RF
     % a1 = (y_ir - b1 RF) / Ts
-    acon = (yir - (bcon.*RF))./ (T-Tx); % relation constant
+    
+    acon = (yir - (bcon.*RF))./ (T - v.atm.Tx); % relation constant with supergreenhouse effect
 end
 
 function bcon = bconstant(yirA,yirM,RFA,RFM)
@@ -326,8 +311,9 @@ yir2pan = ThermalIR(Fbb2pan,Fs);
 %     % log(yir2pan/(a1Ts + b1GF) ) = qlog(2) 
 % % therefore:
 %     % q = log(yir2pan / a1Ts + b1GF) / log(2)
-%     
-q = log(yir2pan ./ (acon.*(T2pan - v.atm.Tx) + bcon.*RF))./log(2); 
+%   
+Tv = (T2pan - v.atm.Tx); 
+q = log(yir2pan ./ (acon.*Tv + bcon.*RF))./log(2); 
 
 
 %% OR: assume the 4.4 increase is true for both eras, and the q factor equilibrates this effect
@@ -386,7 +372,7 @@ function dTdt = tempchange(T,S,yhz,PAN,a,b,RF,v)
 X     = v.ea.sa./(v.oc.m.t.*v.oc.hcap);     % Km2/Wyr; total ocean heat capacity per year
 Fbb   = blackbody(T,v);                     % W/m2; outgoing IR flux, based on temperature
 Fsol  = solarrad(S,v);                      % W/m2; incoming IR flux, based on solar constant
-yir   = ThermalIR(Fbb,Fsol);                % thermal IR opacity, based on incoming-outgoing flux, with NO pressure broadening
+yir   = thermalIR(Fbb,Fsol);                % thermal IR opacity, based on incoming-outgoing flux, with NO pressure broadening
 [q,~] = n2press(T,Fsol,a,b,RF,v);           % N2 pressure broadening factor, based on radiative forcing and solar flux
 nyir  = (PAN.^q).*(a.*T + b.*RF);           % thermal IR opacity, given pressure broadening factor (gamma = N/N_0^q)
 
@@ -406,16 +392,23 @@ TMP = ( (Fs .* ((1-yhz).*(1 + (3/4).*yir) + (yhz./2))) ./ v.const.bol).^(1/4) ;
 
 end
 
-function CS = climatesensitivity(CO2,a,b,v)
+function CS = climatesensitivity(CO2,tp,v)
 % With our prototype parametrization, find the climate sensitivity by doubling CO2
 
-GF_0 = RFInterp(CO2,'CO2',v) ;           % greenhouse forcing modern CO2
-GF_d = RFInterp(2.*CO2,'CO2',v) ;        % greenhouse forcing with doubled CO2
-Teq_0 = (((((v.S.Pref/4).*(1-v.ea.alb))+(v.const.aRF.*GF_0.^v.const.bRF))./v.const.bol).^(1/4)); % equilibrium temp
-Teq_d = (((((v.S.Pref/4).*(1-v.ea.alb))+(v.const.aRF.*GF_d.^v.const.bRF))./v.const.bol).^(1/4)); % equilibrium temp with double CO2
-T_0 = ( (((1-v.ea.alb)*v.S.Pref/4) .* (1 + (3/4).* (a.*(Teq_0 - v.atm.Tx) + b.*GF_0) )) ./ v.const.bol).^(1/4) ; %
-T_d = ( (((1-v.ea.alb)*v.S.Pref/4) .* (1 + (3/4).* (a.*(Teq_d - v.atm.Tx) + b.*GF_d) )) ./ v.const.bol).^(1/4) ; %
+GF_0 = RFInterp(CO2,'CO2',v.bb) ;        % greenhouse forcing modern CO2
+GF_d = RFInterp(2.*CO2,'CO2',v.bb) ;     % greenhouse forcing with doubled CO2
+gfs = {GF_0, GF_d};
 
-CS = T_d - T_0;
+for ig =1:length(gfs)
+   Teq{ig} = equilibtemp(gfs{ig},v);     % find equilib temp
+   yir{ig} = (tp.bT .* gfs{ig}) + (tp.aT .* (Teq{ig} - v.atm.Tx) ); % thermal ir
+   nT{ig} = calctemp((v.S.Pref./4).*(1-v.ea.alb),yir{ig},0,v); % new temperature!
+end
+
+function eqT = equilibtemp(RF,v)
+    eqT = (((((v.S.Pref/4).*(1-v.ea.alb))+(v.bb.a.*RF.^v.bb.b))./v.const.bol).^(1/4)); % equilibrium temp at this RF from pCO2
+end
+
+CS = nT{2} - nT{1};                      % high minus low pCO2 temp change
 
 end
